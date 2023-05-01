c7d1581c315f1a4bc2c6063b7da42056cdc821a5
Leonides Fernando de Oliveira <leonides.fernando@gmail.com>
2023-05-01 12:01:19 +0200

    (jmeter): parameterizing number of users

--- jmeter/runAndGenerateReport.bat
+++ jmeter/runAndGenerateReport.bat
@@ -1,19 +1,23 @@
 @echo off
 
 set currentPath=%cd%
-
+set nUsers=%~1
 
 IF EXIST "%currentPath%\reportResult.jtl" DEL /s /Q "reportResult.jtl" 1>nul
 
 @echo on
 @echo "Removing previous reports data if exists"
 @echo off
-IF EXIST "%currentPath%\report\" RMDIR /Q/S %currentPath%\report\ & @echo "  > report folder REMOVED OK"
-IF EXIST "%currentPath%\jmeter.log" DEL /s /Q "%currentPath%\jmeter.log" 1>nul & @echo "  > jmeter.log folder REMOVED OK"
-IF EXIST "%currentPath%\temp\" RMDIR /Q/S %currentPath%\temp & @echo "  > temp folder REMOVED OK"
+IF EXIST "%currentPath%\report\" RMDIR /Q/S %currentPath%\report\ & @echo "  /> report folder REMOVED OK"
+IF EXIST "%currentPath%\jmeter.log" DEL /s /Q "%currentPath%\jmeter.log" 1>nul & @echo "  /> jmeter.log folder REMOVED OK"
+IF EXIST "%currentPath%\temp\" RMDIR /Q/S %currentPath%\temp & @echo "  /> temp folder REMOVED OK"
+
+IF "%nUsers%"=="" nUsers = 5
+
+@echo %nUsers%
 
 
 @echo on
 @echo "Running JMeter Tests and generating dashboard report"
 @echo off
-jmeter -n -t BackEnd-Perf-Test.jmx -l reportResult.jtl & jmeter -g reportResult.jtl -o report
+jmeter -n -t BackEnd-Perf-Test.jmx -JnUsers -l reportResult.jtl & jmeter -g reportResult.jtl -o report
